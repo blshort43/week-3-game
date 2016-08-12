00a6b938f8d73eaec727ca21a102b8041496acdf
Brandon <blshort43@gmail.com>
2016-08-11 22:21:41 -0500

    fixed pyschic game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,45 +1,50 @@
 
 
-var remainingGuesses = 20;
+var remainingGuesses = 10;
 var wins = 0;
 var losses = 0;
 var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+var computerChoice;
+var userGuess;
+var guessedLetters = [];
 
-
-
-window.onkeyup = function(event) {
+	window.onkeyup = function() {
 	var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
-	var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
-	
+    var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+	console.log(computerChoice);
 
-	if (remainingGuesses >= 0){
-		remainingGuesses--;
+
+	if (userGuess === computerChoice){
+		wins++;
+		remainingGuesses = 9;
+		guessedLetters = [];
+	}else if (remainingGuesses == 1){
+		losses++;
+		remainingGuesses = 9;
+		guessedLetters = [];
+	}else{
+		remainingGuesses --;
+		guessedLetters.push(userGuess);
 	}
 
-	if ((userGuess === computerChoice && remainingGuesses >= 0)){
-		wins++;
-	}else if ((userGuess !== computerChoice && remainingGuesses >= 0)){
-		losses++;
-
-		var letter = "<p>Your Guesses So Far: " + userGuess + ",</p>"
+		// var letter = "<p>Your Guesses So Far: " + guessedLetters + ",</p>"
 		var winNumber = "<p>Wins: " + wins + "</p>";
 		var lossNumber = "<p>Losses: " + losses + "</p>";
 		var guessLeft = "<p>Guesses Left: " + remainingGuesses + "</p>";
-
+		var guessed = "<p>Your Guesses So Far: " + guessedLetters + "</p>"
 
 		// Placing the html into the game ID
-		var div = document.getElementById('guessedLetters');
-		div.innerHTML = div.innerHTML + userGuess + ", ";
+		// var div = document.getElementById('guessedLetters');
+		// div.innerHTML = div.innerHTML + userGuess + ", ";
 
+		document.querySelector('#guessedLetters').innerHTML = guessed;
 		document.querySelector('#wins').innerHTML = winNumber;
 		document.querySelector('#losses').innerHTML = lossNumber;
 		document.querySelector('#guessesLeft').innerHTML = guessLeft;
 	}
-	}
 
 
 
 
 
-	
